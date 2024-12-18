13fdcd94c0ed6184d6fa5fe38484d2763c75e5e1
Socket <xmlrequest@proton.me>
2024-12-18 15:17:35 -0800

    Add files via upload

---
+++ build-config.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import fs from 'fs';
+
+const config = `{
+    entry: './demo.js',
+    output: {
+        filename: 'webpack-output.js',
+        path: '${path.resolve('webpack-output').replace(/\\/g, '\\\\')}',
+    },
+    module: {
+        rules: [
+            {
+                test: /.node$/,
+                loader: 'node-loader',
+            },
+        ],
+    },
+    externals: [
+        ({ context, request }, callback) => {
+            if (/.node$/.test(request)) {
+                return callback(null, 'commonjs ' + request);
+            }
+            callback();
+        },
+    ],
+    target: 'node',
+    mode: 'production',
+}`
+
+fs.writeFileSync('webpack.config.js', `export default ${config}`);
---
+++ demo.js
@@ -0,0 +1,72 @@
+import Imap from 'node-imap';
+import { simpleParser } from 'mailparser';
+
+const combo = 'email@gmail.com:password'
+
+const [email, password] = combo.split(':')
+
+const server = {
+    host: 'imap.gmail.com',
+    port: 993
+}
+
+const imap = new Imap({
+    user: email,
+    password,
+    host: server.host,
+    port: server.port,
+    tls: true
+})
+
+imap.once('ready', () => {
+    console.log('Logged in')
+
+    imap.getBoxes((err, boxes) => {
+        if (err) {
+            console.log('error getting boxes', email)
+        }
+
+        console.log(`Inboxes for ${email}:`, Object.keys(boxes))
+    })
+
+    resolve()
+})
+
+imap.once('error', (err) => {
+    console.log('error', email, err)
+})
+
+imap.connect()
+imap.once('ready', () => {
+    console.log('Logged in');
+
+    imap.openBox('INBOX', true, (err, box) => {
+        if (err) throw err;
+
+        const fetch = imap.seq.fetch('1:10', {
+            bodies: '',
+            struct: true
+        });
+
+        fetch.on('message', (msg, seqno) => {
+            msg.on('body', (stream, info) => {
+                simpleParser(stream, (err, mail) => {
+                    if (err) {
+                        console.log('error parsing email', err);
+                    } else {
+                        console.log('Parsed email:', mail);
+                    }
+                });
+            });
+        });
+
+        fetch.once('error', (err) => {
+            console.log('Fetch error:', err);
+        });
+
+        fetch.once('end', () => {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    });
+});
